f8527b298aac17a43a7174c7f041834b39561f61
Muhammad Shofa <95084317+muhammad-shofa@users.noreply.github.com>
2023-10-21 21:20:38 +0700

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+});
+
+var humberger = document.getElementById("humberger");
+humberger.addEventListener("click", showUl);
+
+var ulNav = document.querySelector("ul");
+function showUl() {
+  const navUl = document.querySelector("ul");
+  const rightIcon = document.getElementById("right-icon");
+  const xIcon = document.getElementById("x-icon");
+  if (navUl.classList.contains("hidden-ul")) {
+    navUl.classList.remove("hidden-ul");
+    navUl.classList.add("show-ul");
+    rightIcon.classList.remove("show-right");
+    rightIcon.classList.add("hidden-right");
+    xIcon.classList.remove("hidden-x");
+    xIcon.classList.add("show-x");
+  } else {
+    navUl.classList.remove("show-ul");
+    navUl.classList.add("hidden-ul");
+    rightIcon.classList.add("show-right");
+    rightIcon.classList.remove("hidden-right");
+    xIcon.classList.add("hidden-x");
+    xIcon.classList.remove("show-x");
+  }
+}
